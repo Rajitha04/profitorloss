a05da568f4dcdc99586a697f5975a338c832c80c
Rajitha Muppalla Penchala <mprajitha041098@gmail.com>
2021-10-12 00:26:38 +0530

    final commit

--- index.js
+++ index.js
@@ -6,20 +6,27 @@ var output=document.querySelector("#output")
 
 
 function profitlosscalculator(intial,number,current){
+    if(intial ===0 || number===0 || current===0){
+        showmessage(`please enter all the details`)
+    }
+    else{
     if(intial>current){
-var loss=(intial-current)*number;
+var loss=Math.trunc((intial-current)*number);
 var losspercentage=(loss/intial)*100;
+var losspercentagenumber=losspercentage.toFixed(2);
 
-showmessage(`your loss is ${loss} and loss percentage is ${losspercentage}%`);
+showmessage(`your loss is ${loss} and loss percentage is ${losspercentagenumber}%`);
     }
     else if(intial<current){
-var profit=(current-intial)*number;
+var profit=Math.trunc((current-intial)*number);
 var profitpercentage=(profit/intial)*100;
-showmessage(`your profit is ${profit} and profit percentage is ${profitpercentage}%`);
+var profitpercentagenumber=profitpercentage.toFixed(2);
+showmessage(`your profit is ${profit} and profit percentage is ${profitpercentagenumber}%`);
     }
     else{
 showmessage("no loss no profit");
     }
+}
 }
 function showmessage(message){
     output.innerHTML=message;
